e411b9bb37d6a57b6b22c7ebee943d776b2264a6
CVC_DM\mtzcpd1268 <rafaeldocarmo@ext.cvc.com.br>
2017-12-11 20:35:19 -0200

    day2

--- loto.js
+++ loto.js
@@ -1,67 +1,85 @@
-const request = require("request");
-const fs = require('fs');
-const AdmZip = require("adm-zip");
-const tabletojson = require('tabletojson');
-const jsonfile = require('jsonfile');
-const argv = require('minimist')(process.argv.slice(2));
+const request = require("request")
+const fs = require('fs')
+const AdmZip = require("adm-zip")
+const tabletojson = require('tabletojson')
+const jsonfile = require('jsonfile')
+const argv = require('minimist')(process.argv.slice(2))
+const _ = require('lodash')
 
-const filePath = "./files";
-const zipPath = filePath + "/loto.zip";
-const jsonPath = filePath + "/data.json";
+const filePath = "./files"
+const zipPath = filePath + "/loto.zip"
+const jsonPath = filePath + "/data.json"
 
 console.log("ficar vendo ",argv)
+console.info('####  INICIO  ####')
 
 function downloadLotoFile() {
-    let options = {
-        method: 'GET',
-        url: 'http://www1.caixa.gov.br/loterias/_arquivos/loterias/D_lotfac.zip',
-        headers: {
-            'cache-control': 'no-cache',
-            cookie: '_pk_ref.4.968f=%5B%22%22%2C%22%22%2C1507568980%2C%22https%3A%2F%2Fwww.google.com.br%2F%22%5D; _pk_id.4.968f=ca07f4ef4dde32ff.1507568980.1.1507568990.1507568980.; _pk_ses.4.968f=*; _ga=GA1.3.750979606.1507568990; _gid=GA1.3.211084725.1507568990; security=true',
-            'accept-encoding': 'gzip, deflate',
-            referer: 'http://resultadolotofacil.org/download-resultados/'
-        }
-    }
+    let options = { method: 'GET',
+    url: 'http://www1.caixa.gov.br/loterias/_arquivos/loterias/D_lotfac.zip',
+    headers: 
+     { 'postman-token': '2c3bde59-d348-d958-91c2-a02904a58c2d',
+       'cache-control': 'no-cache',
+       cookie: 'security=true',
+       'accept-language': 'pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7',
+       'accept-encoding': 'gzip, deflate',
+       accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+       'upgrade-insecure-requests': '1',
+       'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36' } };
     return new Promise(function (resolve) {    
-        if(argv.nd && (fs.existsSync(zipPath) || fs.existsSync(jsonPath))) resolve();    
-        console.info("Iniciando download do arquivo");
-        if(fs.existsSync(zipPath)) fs.unlink(zipPath);
+        if(argv.nd && (fs.existsSync(zipPath) || fs.existsSync(jsonPath))){
+            console.info("Não foi necessário fazer download")
+            return resolve()    
+        } 
+        console.info("Iniciando download do arquivo")
+        if(fs.existsSync(zipPath)) fs.unlink(zipPath)
         request(options)
             .pipe(fs.createWriteStream(zipPath))
             .on('close', function () {
-                console.info("Finalizando download do arquivo");
-                resolve();
-            });
+                console.info("Finalizando download do arquivo")
+                resolve()
+            })
     })
 }
 function tratarZipdataParaJSON() {
-    var zipEntries = new AdmZip(zipPath).getEntries();
-    var zipdata = removerAcentos(zipEntries[0].getData().toString());
-    var tablesAsJson = tabletojson.convert(zipdata);
-    var file = jsonPath
-    jsonfile.writeFile(file, { "completo": tablesAsJson[0] }, { spaces: 2, EOL: '\r\n', flag: 'a' })
+    return new Promise(function(resolve){
+        var zipEntries = new AdmZip(zipPath).getEntries()
+        var zipdata = removerAcentos(zipEntries[0].getData().toString())
+        var tablesAsJson = tabletojson.convert(zipdata)
+        jsonfile.writeFile(jsonPath, { "completo": tablesAsJson[0] }, { spaces: 2, EOL: '\r\n', flag: 'a' })
+        resolve()
+    })
 }
 function alimentarDB() {
+    console.info("Inserindo informações no DB")
     if(!argv.db) return false
-    const mongoose = require('mongoose');
-    mongoose.connect('mongodb://localhost/test', { useMongoClient: true });
-    mongoose.Promise = global.Promise;
-    
-    var thingSchema = new mongoose.Schema({}, { strict: false });
-    var Sorteio = mongoose.model('Sorteio',thingSchema);
-    
-    var resultado = new Sorteio({"meme":"red","sagui":true});
-    return resultado.save();
+    return new Promise(function(resolve){
+        const mongoose = require('mongoose')
+        mongoose.connect('mongodb://localhost/test', { useMongoClient: true })
+        mongoose.Promise = global.Promise
+        var thingSchema = new mongoose.Schema({}, { strict: false })
+        var Sorteio = mongoose.model('Sorteio',thingSchema)
+        jsonfile.readFile(jsonPath, function(err, obj) {
+            obj.completo.forEach(element => {
+                console.log(element)
+                var resultado = new Sorteio({"meme":"red","sagui":true})
+                resultado.save()
+            })
+            resolve()
+        })
+        console.info("DB OK")
+    })
 }
 function removerArquivos() {
-    fs.unlink(jsonPath);
-    if(!argv.m){
-        fs.unlink(zipPath);
+    console.info("Removendo arquivo JSON")
+    fs.unlink(jsonPath)
+    if(!argv.m && !argv.nd){
+        console.info("Removendo arquivo ZIP")
+        fs.unlink(zipPath)
     }
-    return true;
+    return true
 }
 function removerAcentos(newStringComAcento) {
-    var string = newStringComAcento;
+    var string = newStringComAcento
     var mapaAcentosHex = {
         a: /[\xE0-\xE6]/g,
         A: /[\xC0-\xC6]/g,
@@ -77,23 +95,23 @@ function removerAcentos(newStringComAcento) {
         C: /\xC7/g,
         n: /\xF1/g,
         N: /\xD1/g,
-    };
-
-    for (var letra in mapaAcentosHex) {
-        var expressaoRegular = mapaAcentosHex[letra];
-        string = string.replace(expressaoRegular, letra);
     }
 
-    return string;
+    for (var letra in mapaAcentosHex) {
+        var expressaoRegular = mapaAcentosHex[letra]
+        string = string.replace(expressaoRegular, letra)
+    }
+
+    return string
 }
 
 function executar() {
     return downloadLotoFile()
         .then(tratarZipdataParaJSON)
-        .then(removerArquivos)
         .then(alimentarDB)
+        .then(removerArquivos)
 }
 
 executar().then(function(){
-    console.log('pronto')
+    console.info('####  FIM  ####')
 })
