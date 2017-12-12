f3fad127f1e1c1405998d9b5dbb17e2ca7184358
CVC_DM\mtzcpd1268 <rafaeldocarmo@ext.cvc.com.br>
2017-12-12 16:40:37 -0200

    Adicionado progressbar Mudado o download apenas passando a como parametro Banco de dados alimentado

--- loto.js
+++ loto.js
@@ -26,9 +26,9 @@ function downloadLotoFile() {
        'upgrade-insecure-requests': '1',
        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36' } };
     return new Promise(function (resolve) {    
-        if(argv.nd && (fs.existsSync(zipPath) || fs.existsSync(jsonPath))){
+        if(!argv.a && (fs.existsSync(zipPath) || fs.existsSync(jsonPath))){
             console.info("Não foi necessário fazer download")
-            return resolve()    
+            return resolve()
         } 
         console.info("Iniciando download do arquivo")
         if(fs.existsSync(zipPath)) fs.unlink(zipPath)
@@ -43,66 +43,53 @@ function downloadLotoFile() {
 function tratarZipdataParaJSON() {
     return new Promise(function(resolve){
         var zipEntries = new AdmZip(zipPath).getEntries()
-        var zipdata = removerAcentos(zipEntries[0].getData().toString())
-        var tablesAsJson = tabletojson.convert(zipdata)
+        // var zipdata = removerAcentos(zipEntries[0].getData().toString())
+        // var tablesAsJson = tabletojson.convert(zipdata)
+        var tablesAsJson = tabletojson.convert(zipEntries[0].getData().toString())
         jsonfile.writeFile(jsonPath, { "completo": tablesAsJson[0] }, { spaces: 2, EOL: '\r\n', flag: 'a' })
         resolve()
     })
 }
 function alimentarDB() {
-    console.info("Inserindo informações no DB")
+    console.info("Iniciando DB")
     if(!argv.db) return false
-    return new Promise(function(resolve){
+    console.info("Inserindo informações no DB")
+    return new Promise(function(resolve,reject){
         const mongoose = require('mongoose')
+        const _progress = require('cli-progress')
+        var bar1 = new _progress.Bar({
+            format: '{ inserindo no banco [{bar}] {percentage}% | {value}/{total} }'
+        }, _progress.Presets.shades_classic);
         mongoose.connect('mongodb://localhost/test', { useMongoClient: true })
         mongoose.Promise = global.Promise
         var thingSchema = new mongoose.Schema({}, { strict: false })
         var Sorteio = mongoose.model('Sorteio',thingSchema)
         jsonfile.readFile(jsonPath, function(err, obj) {
-            obj.completo.forEach(element => {
-                console.log(element)
-                var resultado = new Sorteio({"meme":"red","sagui":true})
-                resultado.save()
-            })
-            resolve()
+            try {
+                bar1.start(obj.completo.length, 0);
+                obj.completo.forEach(function(element,i){
+                    bar1.update(i);
+                    var resultado = new Sorteio(JSON.parse(JSON.stringify(element)))
+                    resultado.save()
+                })                
+                bar1.stop();
+                resolve()
+            } catch (e) {
+                console.log("not JSON", e);
+                reject(e)
+            }
         })
         console.info("DB OK")
     })
 }
 function removerArquivos() {
+    return true
     console.info("Removendo arquivo JSON")
     fs.unlink(jsonPath)
-    if(!argv.m && !argv.nd){
+    if(!argv.m){
         console.info("Removendo arquivo ZIP")
         fs.unlink(zipPath)
     }
-    return true
-}
-function removerAcentos(newStringComAcento) {
-    var string = newStringComAcento
-    var mapaAcentosHex = {
-        a: /[\xE0-\xE6]/g,
-        A: /[\xC0-\xC6]/g,
-        e: /[\xE8-\xEB]/g,
-        E: /[\xC8-\xCB]/g,
-        i: /[\xEC-\xEF]/g,
-        I: /[\xCC-\xCF]/g,
-        o: /[\xF2-\xF6]/g,
-        O: /[\xD2-\xD6]/g,
-        u: /[\xF9-\xFC]/g,
-        U: /[\xD9-\xDC]/g,
-        c: /\xE7/g,
-        C: /\xC7/g,
-        n: /\xF1/g,
-        N: /\xD1/g,
-    }
-
-    for (var letra in mapaAcentosHex) {
-        var expressaoRegular = mapaAcentosHex[letra]
-        string = string.replace(expressaoRegular, letra)
-    }
-
-    return string
 }
 
 function executar() {
